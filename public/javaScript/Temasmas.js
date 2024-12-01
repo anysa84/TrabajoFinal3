b62beb4afc9e862aa4e9440987e9335d08392c10
anysa84 <anahi.vsa@gmail.com>
2024-12-01 13:30:08 -0300

    Add files via upload

--- public/javaScript/Temasmas.js
+++ public/javaScript/Temasmas.js
@@ -1,11 +1,16 @@
 //agregar tema al Album
-
 document.getElementById('addSongButton').addEventListener('click', function() {
+  const nameInput = document.getElementById('nameInput');
   const songInput = document.getElementById('songInput');
+  const songName = nameInput.value.trim();
   const songURL = songInput.value.trim();
 
-  if (songURL) {
+  if (songName && songURL) {
       const listItem = document.createElement('li');
+      
+      const songNameElement = document.createElement('span');
+      songNameElement.textContent = songName + " - ";
+
       const songLink = document.createElement('a');
       songLink.href = songURL;
       songLink.target = '_blank'; // Abrir en una nueva pestaña
@@ -13,16 +18,19 @@ document.getElementById('addSongButton').addEventListener('click', function() {
       songLink.textContent = songURL;
 
       const deleteButton = document.createElement('button');
-      deleteButton.textContent = '.                 - Eliminar';
+      deleteButton.textContent = '.                               -Eliminar';
       deleteButton.addEventListener('click', function() {
           listItem.remove();
       });
 
+      listItem.appendChild(songNameElement);
       listItem.appendChild(songLink);
       listItem.appendChild(deleteButton);
       document.getElementById('musicList').appendChild(listItem);
-      songInput.value = ''; // Limpiar el campo de entrada
+
+      nameInput.value = ''; // Limpiar el campo de entrada de nombre
+      songInput.value = ''; // Limpiar el campo de entrada de URL
   } else {
-      alert('Por favor, introduce una URL válida.');
+      alert('Por favor, introduce un nombre y una URL válida.');
   }
 });
