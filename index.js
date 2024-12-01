748f2522af8e699b51cbcea7c2b50fc880868806
anysa84 <anahi.vsa@gmail.com>
2024-12-01 11:34:30 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+//busca los .htlm
+const path = require("path");
+app.use(express.static(path.join(__dirname, "./public")));
+
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conectado a MongoDB'))
